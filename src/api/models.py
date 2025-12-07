647e8e80e56062ca193e1b23997ccfac321e784a
Fabian993 <fabiantrummer123@gmail.com>
2025-12-07 16:10:04 +0100

    #20: Made and migrated first models

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,10 @@
+"""
+Docstring for api.models
+
+Source for SQL-Django Syntax:
+https://docs.djangoproject.com/en/6.0/ref/models/fields/
+"""
+
 from django.db import models  # noqa
 
 
@@ -8,3 +15,33 @@ class TestModel(models.Model):
 
     def __str__(self):
         return self.name
+
+class School(models.Model):
+    name = models.CharField(max_length=255)
+    adress = models.CharField(max_length=255)
+
+class User(models.Model):
+    school_id = models.ForeignKey(School, on_delete=models.CASCADE)
+    username = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password_hash = models.BinaryField(max_length=32) #SHA-256
+    created_at = models.DateField
+
+class Class(models.Model):
+    teacher_id = models.ForeignKey("Teacher", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+class Teacher(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    class_id = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+class Student(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    class_id = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+class Parent(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    children = models.ManyToManyField(Student)
+
