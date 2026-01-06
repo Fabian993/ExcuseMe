0c8f82dd3c466268be9ef3c8b0f7861b87720de8
Jan <jaschubert04@gmail.com>
2026-01-06 14:48:21 +0100

    #28: moved django / api related files to backend

---
+++ backend/src/api/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+#from api.models import TestModel
+#
+#
+#@admin.register(TestModel)
+#class TestModelAdmin(admin.ModelAdmin):
+#    list_display = ("name", "description")
+#
