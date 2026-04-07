a6524c8a2cd8b8e25ebda601ad86de072b6ed739
Atlas-Zero <jaschubert04@gmail.com>
2026-04-07 22:07:20 +0200

    #46 excuses working

--- excuseme/lib/pages/excuses_page.dart
+++ excuseme/lib/pages/excuses_page.dart
@@ -3,6 +3,33 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:dio/dio.dart';
 import 'package:excuseme/models/storage.dart';
 
+Future<List<dynamic>> getExcuses() async {
+  final StorageManager sm = StorageManager();
+  final Dio dio = Dio();
+  // print(sm.tokens!.access);
+  try {
+    String? backendAddress = dotenv.env['BACKEND_SERVER'];
+    String? bearer = await sm.storage.read(key: 'access');
+
+    final response = await dio.get(
+      'https://$backendAddress/api/excuses/',
+      options: Options(
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $bearer',
+        },
+      ),
+    );
+    List<dynamic> excuses = response.data['results'];
+
+    return excuses;
+  } catch (e) {
+    return [
+      {'Error': e},
+    ];
+  }
+}
+
 class ExcusesPage extends StatefulWidget {
   const ExcusesPage({super.key});
 
@@ -11,50 +38,48 @@ class ExcusesPage extends StatefulWidget {
 }
 
 class _ExcusesPageState extends State<ExcusesPage> {
-  dynamic excuses = '';
-  final StorageManager sm = StorageManager();
-  final Dio dio = Dio();
-
-  Future<Map<String, dynamic>> getExcuses() async {
-    // print(sm.tokens!.access);
-    try {
-      String? backendAddress = dotenv.env['BACKEND_SERVER'];
-      String bearer = sm.tokens!.access;
-
-      final response = await dio.get(
-        'https://$backendAddress/api/excuses/',
-        options: Options(
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer $bearer',
-          },
-        ),
-      );
-      // if (response.statusCode == 200Bearer) {
-      //   return response.data;
-      // }
-      return response.data;
-    } catch (e) {
-      return {'Error': e};
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      spacing: 20,
-      children: [
-        ElevatedButton(
-          onPressed: () async {
-            dynamic response = await getExcuses();
-            setState(() {
-              excuses = response['results'];
-            });
+    return FutureBuilder<List<dynamic>>(
+      future: getExcuses(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        }
+
+        if (snapshot.hasError) {
+          return SelectableText("Error: ${snapshot.error}");
+        }
+
+        if (!snapshot.hasData) {
+          return const Text("No data.\nNo, that's a good thing.");
+        }
+
+        return ListView.builder(
+          itemCount: snapshot.data!.length,
+          itemBuilder: (context, index) {
+            final excuse = snapshot.data![index];
+            return Card(
+              margin: const EdgeInsets.all(10),
+              child: ListTile(
+                contentPadding: const EdgeInsets.all(10),
+                title: Text("Excuse ID: ${excuse['id']} - ${excuse['title']}"),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Date: ${excuse['created_at']}"),
+                    Text("Content: ${excuse['content']}"),
+                    Text(
+                      "User: ${excuse['uploaded_by_user'].toString().split(':')[1].split(',')[0]}",
+                    ),
+                    SelectableText(excuse.toString()),
+                  ],
+                ),
+              ),
+            );
           },
-          child: Text('Get Excuses'),
-        ),
-        SelectableText('Fetched Excuses: $excuses'),
-      ],
+        );
+      },
     );
   }
 }
