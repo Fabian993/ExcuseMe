d390b8e145936b34ec0efc9e338b3753df6317cf
Atlas-Zero <jaschubert04@gmail.com>
2026-06-23 00:35:31 +0200

    add theme_controller

--- excuseme/lib/main.dart
+++ excuseme/lib/main.dart
@@ -1,8 +1,12 @@
-import 'package:excuseme/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:excuseme/pages/login_page.dart';
+import 'package:excuseme/components/theme_controller.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(create: (_) => ThemeController(), child: MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,29 +15,37 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ExcuseMe',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Color(0x000066FF),
-          primary: Colors.deepOrange,
-          secondary: Color(0x001a0201),
-          brightness: Brightness.light,
-        ),
-      ),
-      darkTheme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Color(0x000066FF),
-          primary: Colors.deepOrange,
-          secondary: Color(0x001a0201),
-          brightness: Brightness.dark,
-        ),
-      ),
-      themeMode: ThemeMode.system,
-      home: LoginPage(),
+    final controller = context.watch<ThemeController>();
+
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, _) {
+        return MaterialApp(
+          title: 'ExcuseMe',
+          debugShowCheckedModeBanner: false,
+
+          theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Color(0x000066FF),
+              primary: Colors.deepOrange,
+              secondary: Color(0x001a0201),
+              brightness: Brightness.light,
+            ),
+          ),
+          darkTheme: ThemeData(
+            useMaterial3: true,
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Color(0x000066FF),
+              primary: Colors.deepOrange,
+              secondary: Color(0x001a0201),
+              brightness: Brightness.dark,
+            ),
+          ),
+          themeMode: controller.themeMode,
+          home: LoginPage(),
+        );
+      },
     );
   }
 }
